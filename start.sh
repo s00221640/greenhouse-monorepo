77406e36f4f4493b403ca8bea0eb6a98475c6190
Jack Tierney <s00221640@atu.ie>
2025-04-10 05:23:18 +0100

    npm

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd server
 npm install
+
 cd ../client
-npm install
-ng build --output-path=../server/dist/client
+npm install --legacy-peer-deps
+npx ng build --output-path=../server/dist/client
+
 cd ../server
 node dist/app.js
