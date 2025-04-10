6e6c145ee996ee2f295fb1a3eb7590bf6dc41324
Jack Tierney <s00221640@atu.ie>
2025-04-10 06:17:52 +0100

    proc

--- start.sh
+++ start.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-# Install backend
+echo "Installing server dependencies..."
 cd server/Greenhouse_App_Part3-serverside-main
 npm install
 
-# Install frontend
+echo "Installing client dependencies..."
 cd ../../client/Greenhouse_App_Part3-clientside-main/greenhouse-app-part2
 npm install
 
-# Build Angular
-npx ng build --output-path=../../../../server/Greenhouse_App_Part3-serverside-main/dist/client
+echo "Building Angular app..."
+npx ng build --output-path=../../../server/Greenhouse_App_Part3-serverside-main/dist/client
 
-# Start backend
-cd ../../../../server/Greenhouse_App_Part3-serverside-main
+echo "Building TypeScript server..."
+cd ../../../server/Greenhouse_App_Part3-serverside-main
 npm run build
+
+echo "Starting server..."
 node dist/app.js
