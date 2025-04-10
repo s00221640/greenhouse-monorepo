d67037b86316d8bcf63edd839e23baf188773bdc
Jack Tierney <s00221640@atu.ie>
2025-04-10 05:38:20 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cd server
+cd server/Greenhouse_App_Part3-serverside-main
 npm install
-
-cd ../client/Greenhouse_App_Part3-clientside-main/greenhouse-app-part2
-npm install --legacy-peer-deps
-npx ng build --output-path=../../../server/Greenhouse_App_Part3-serverside-main/dist/client
-
+cd ../../client/Greenhouse_App_Part3-clientside-main/greenhouse-app-part2
+npm install
+npm run build -- --output-path=../../../server/Greenhouse_App_Part3-serverside-main/dist/client
 cd ../../../server/Greenhouse_App_Part3-serverside-main
 node dist/app.js
