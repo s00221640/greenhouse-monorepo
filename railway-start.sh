ee62bcd92fb028eba12e97c28224570a61307cfd
Jack Tierney <s00221640@atu.ie>
2025-04-10 07:08:33 +0100

    Update railway-start.sh

--- railway-start.sh
+++ railway-start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
-# Install only server dependencies (we don't need to build Angular in production)
 echo "Installing server dependencies..."
 cd server/Greenhouse_App_Part3-serverside-main
 npm install --omit=dev
 
-# Build TypeScript server
+echo "Installing Angular dependencies..."
+cd ../../client/Greenhouse_App_Part3-clientside-main/greenhouse-app-part2
+npm install --omit=dev
+
+echo "Building Angular app..."
+npx ng build --output-path=../../../server/Greenhouse_App_Part3-serverside-main/dist/client
+
 echo "Building server..."
+cd ../../../server/Greenhouse_App_Part3-serverside-main
 npm run build
 
-# Start the server directly
 echo "Starting server..."
 node dist/app.js
